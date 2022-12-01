0053d6e7d8b328619b5262fde76c56fae7756aef
kalculata <nprincehuzaifa@gmail.com>
2022-12-01 12:48:43 +0100

    Implement binray search with recursion

---
+++ search_algorithms/binary_search_recursion_1.py
@@ -0,0 +1,18 @@
+def binarySearch(list, target, first, last):
+  """
+	Returns the index position of the target if found, else returns None
+	"""
+  if len(list) == 0:
+    return False
+  
+  midpoint = (first + last) // 2
+  
+  if list[midpoint] == target:
+    return midpoint
+  elif list[midpoint] > target:
+    return binarySearch(list, target, first, midpoint - 1)
+  elif list[midpoint] < target:
+    return binarySearch(list, target, midpoint + 1, last)
+  
+  return None
+		
---
+++ search_algorithms/binary_search_recursion_2.py
@@ -0,0 +1,17 @@
+def binarySearch(list, target):
+  """
+	Returns the index position of the target if found, else returns None
+	"""
+  print(list)
+  if len(list) == 0:
+    return False
+  else:
+    midpoint = len(list) // 2
+    
+    if list[midpoint] == target:
+      return True
+    elif list[midpoint] > target:
+      return binarySearch(list[:midpoint], target)
+    elif list[midpoint] < target:
+      return binarySearch(list[midpoint+1:], target)
+		
