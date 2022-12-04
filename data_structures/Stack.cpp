eea963b5a97c7cb6071dc429a066af8dcf6d9105
kalculata <nprincehuzaifa@gmail.com>
2022-12-04 18:32:58 +0100

    Problem: Reverse string using Stack

--- data_structures/Stack.cpp
+++ data_structures/Stack.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <cstdlib>
+#include <cxxabi.h>
+#include <sstream>
 #include "LinkedList.cpp"
 
 using namespace std;
@@ -6,17 +9,28 @@ using namespace std;
 /// @brief All operations are performed in O(1) time.
 template <typename T> class Stack {
   public:
+    string type;
+
+    Stack() {
+      type = getType();
+    }
+
     friend ostream& operator << (ostream& output, Stack& stack) {
       string repr = "[";
       if(stack.elements.isNotEmpty()) {
         for(int i=0; i<stack.elements.length; i++) {
-          repr += to_string(stack.elements.get(i));
+          if(stack.type == "string") {
+            repr += stack.elements.get(i);
+          } else {
+            stringstream ss;
+            ss << stack.elements.get(i);         
+            repr += ss.str();
+          }
 
           if(i+1 < stack.elements.length) repr += ", ";
         }
       }
       repr += "]";
-
       output << repr;
       return output;
     }
@@ -43,17 +57,33 @@ template <typename T> class Stack {
 
   private:
     LinkedList<T> elements;
+
+    std::string getType() {
+      int status;
+      std::string tname = typeid(T).name();
+      char *demangled_name = abi::__cxa_demangle(tname.c_str(), NULL, NULL, &status);
+
+      if(status == 0) {
+        tname = demangled_name;
+        std::free(demangled_name);
+      } 
+
+      if(tname == "std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> >")  {
+        return "string";
+      }
+      return tname;
+    }
 };
 
 // int main() {
-//   Stack<int> stack;
+//   Stack<string> stack;
 
-//   stack.push(1);
-//   stack.push(2);
-//   stack.push(5);
+//   stack.push("1");
+//   stack.push("2");
+//   stack.push("5");
 
 //   cout << stack << endl;
 
-//   stack.pop();
-//   cout << stack;
+//   // stack.pop();
+//   // cout << stack << endl;
 // }
--- problems/reverse_string.cpp
+++ problems/reverse_string.cpp
@@ -1,16 +1,24 @@
 #include <iostream>
 #include <string>
-
+#include <sstream>
 #include "../data_structures/Stack.cpp"
 
 using namespace std;
 
+string char_to_string(char c) {
+  std::string str;
+  std::stringstream strStream;
+  strStream << c;
+  strStream >> str;
+  return str;
+}
+
 int main() {
-  Stack reverseStr; 
+  Stack<string> reverseStr; 
   string str = "Huzaifa Nimushimirimana";
 
   for(int i=0; i<str.length(); i++) {
-    reverseStr.push(str[i]);
+    reverseStr.push(char_to_string(str[i]));
   }
 
   cout << reverseStr;
